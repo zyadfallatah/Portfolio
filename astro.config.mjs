ee35f542ddcd96415a8f837e4192b48b206c6c04
zyadfallatah <zyadfallatah@gmail.com>
2025-07-23 22:34:07 +0300

    Welcome Part Finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    locales: ["en", "ar"],
+    defaultLocale: "ar",
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
