42721aaebe035ad770c75352b1b6da567784b824
zyadfallatah <zyadfallatah@gmail.com>
2025-07-28 20:03:11 +0300

    Many changes: project page + removed min-h-svh + handled responsive issues

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,11 @@
 interface Project {
   title: string;
   shortDescription: string;
+  longDescription?: string;
   technologies: { name: string; icon: string }[];
   metadata: string[];
   bannar: string;
+  tags?: string[];
+  linkName?: string;
+  content?: string;
 }
---
+++ src/constants/index.ts
@@ -0,0 +1,38 @@
+export const projects: Project[] = [
+  {
+    linkName: "mulabbi",
+    title: "ملبي",
+    shortDescription: `تطبيق يرشد الحاج أو المعتمر\nلأداء مناسكه خطوة بخطوة`,
+    technologies: [
+      { name: "flutter", icon: "/assets/flutter.svg" },
+      { name: "supabase", icon: "/assets/Supabase.svg" },
+    ],
+    metadata: ["المنصة: تطبيق جوال "],
+    tags: ["عمل جماعي", "إسلامي", "إرشادي"],
+    bannar: "/images/mulabbi-banner.svg",
+  },
+  {
+    linkName: "devflow",
+    title: "DevFlow",
+    shortDescription: `موقع تواصل و أسئلة 
+بين المطورين`,
+    technologies: [
+      { name: "React", icon: "/assets/react.svg" },
+      { name: "Tailwind", icon: "/assets/tailwindcss.svg" },
+      { name: "MongoDB", icon: "/assets/Mongodb.svg" },
+    ],
+    metadata: ["المنصة: تطبيق ويب "],
+    bannar: "/images/devflow-banner.svg",
+  },
+];
+
+export const hardSkills: { name: string; icon: string }[] = [
+  { name: "react", icon: "/assets/react.svg" },
+  { name: "react", icon: "/assets/react.svg" },
+  { name: "react", icon: "/assets/react.svg" },
+];
+
+export const softSkills: { name: string; icon?: string }[] = [
+  { name: "react" },
+  { name: "react", icon: "/assets/react.svg" },
+];
