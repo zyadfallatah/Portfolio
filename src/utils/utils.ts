a1a310aa5634d351b69d71d78a9e6d883915cf9d
zyadfallatah <zyadfallatah@gmail.com>
2025-08-04 09:15:35 +0300

    Refactor: project constant changed into collections

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,14 +1,18 @@
 // cn.ts
+import { getCollection } from "astro:content";
 import type { ClassValue } from "clsx";
 
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { projects } from "../constants";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
 
-export const getCurrentProject = (name: string) => {
-  return projects.filter((project) => project.linkName === name).at(0) || null;
+export const getCurrentProject = async (name: string) => {
+  const projects = await getCollection("projects");
+  const currentProject = projects
+    .filter(({ data: project }) => project.linkName === name)
+    .at(0);
+  return currentProject?.data || null;
 };
